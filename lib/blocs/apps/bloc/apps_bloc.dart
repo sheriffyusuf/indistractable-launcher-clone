0d01e9437f48582c73c3e0d8125563dba8e31022
Sheriff Yusuf <sherifdeenmuhammed@outlook.com>
2020-04-25 12:52:33 +0100

    update app_bloc

--- lib/blocs/apps/bloc/apps_bloc.dart
+++ lib/blocs/apps/bloc/apps_bloc.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:device_apps/device_apps.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:indistractable_clone/blocs/apps/bloc/apps.dart';
+import 'package:indistractable_clone/models/App.dart';
 
-class AppsBloc extends Bloc<AppsEvent, AppsState> {
+class AppsBloc extends HydratedBloc<AppsEvent, AppsState> {
   @override
-  AppsState get initialState => AppsLoadInProgress();
+  AppsState get initialState => super.initialState ?? AppsLoadInProgress();
 
   @override
   Stream<AppsState> mapEventToState(
@@ -18,8 +20,20 @@ class AppsBloc extends Bloc<AppsEvent, AppsState> {
 
   Stream<AppsState> _mapAppsLoadedToState() async* {
     try {
-      final apps = await DeviceApps.getInstalledApplications(
+      final appsList = await DeviceApps.getInstalledApplications(
           onlyAppsWithLaunchIntent: true, includeSystemApps: true);
+      final apps = appsList
+          .map((app) => App(
+              appName: app.appName,
+              apkFilePath: app.apkFilePath,
+              packageName: app.packageName,
+              versionName: app.versionName,
+              versionCode: app.versionCode,
+              dataDir: app.dataDir,
+              systemApp: app.systemApp,
+              installTimeMilis: app.installTimeMilis,
+              updateTimeMilis: app.updateTimeMilis))
+          .toList();
       apps.sort(
           (a, b) => a.appName.toLowerCase().compareTo(b.appName.toLowerCase()));
       yield AppsLoaded(
@@ -28,5 +42,33 @@ class AppsBloc extends Bloc<AppsEvent, AppsState> {
     } catch (_) {
       yield AppsLoadFailure();
     }
+  }
+
+  @override
+  AppsState fromJson(Map<String, dynamic> json) {
+    // print(jsonDecode(json['apps']).toString());
+    // List<App> apps = jsonDecode(json['apps']).map((app) {
+    // print(app);
+    // return App.fromMap(app);
+    // }).toList();
+    //print(apps.toString());
+    // print(apps.last.toString());
+    //  print(jsonDecode(json['apps']).toString());
+    //List<App> apps = Apps.fromJson(json).apps;
+    // print(apps.toString());
+    List<App> apps =
+        jsonDecode(json['apps']).map<App>((app) => App.fromJson(app)).toList();
+    print(apps.last.appName.toString());
+    //print(apps.last.toString());
+    return AppsLoaded(apps: Apps.fromJson(json).apps);
+  }
+
+  @override
+  Map<String, dynamic> toJson(AppsState state) {
+    //jsonEncode(state.apps.map((e) => e.toJson()).toList())
+    if (state is AppsLoaded)
+      return {'apps': jsonEncode(state.apps)};
+    else
+      return null;
   }
 }
