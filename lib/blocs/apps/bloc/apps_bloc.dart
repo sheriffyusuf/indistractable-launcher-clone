d709843c56c2280f7e232937cc5f152c8426dc66
Sheriff Yusuf <sherifdeenmuhammed@outlook.com>
2020-04-29 16:53:38 +0100

    Some code cleanup

--- lib/blocs/apps/bloc/apps_bloc.dart
+++ lib/blocs/apps/bloc/apps_bloc.dart
@@ -46,26 +46,14 @@ class AppsBloc extends HydratedBloc<AppsEvent, AppsState> {
 
   @override
   AppsState fromJson(Map<String, dynamic> json) {
-    // print(jsonDecode(json['apps']).toString());
-    // List<App> apps = jsonDecode(json['apps']).map((app) {
-    // print(app);
-    // return App.fromMap(app);
-    // }).toList();
-    //print(apps.toString());
-    // print(apps.last.toString());
-    //  print(jsonDecode(json['apps']).toString());
-    //List<App> apps = Apps.fromJson(json).apps;
-    // print(apps.toString());
     List<App> apps =
         jsonDecode(json['apps']).map<App>((app) => App.fromJson(app)).toList();
     print(apps.last.appName.toString());
-    //print(apps.last.toString());
     return AppsLoaded(apps: Apps.fromJson(json).apps);
   }
 
   @override
   Map<String, dynamic> toJson(AppsState state) {
-    //jsonEncode(state.apps.map((e) => e.toJson()).toList())
     if (state is AppsLoaded)
       return {'apps': jsonEncode(state.apps)};
     else
